7139a8aa1a8463db630fd533a95368d220f29330
sujithkumar216802 <sujithkumar216802@gmail.com>
2021-08-22 21:49:10 +0530

    change extractor

--- lib/utils/extractor.dart
+++ lib/utils/extractor.dart
@@ -3,57 +3,46 @@ import 'dart:convert';
 import 'package:insta_downloader/models/file_info_model.dart';
 
 class Extractor {
-  static const jsonHeader = [
-    '<script type="application/ld+json">',
-    'window._sharedData = '
-  ];
+  static const jsonHeader = 'window._sharedData = ';
 
-  static const jsonFooter = ['</script>', ';</script>'];
+  static const jsonFooter = ';</script>';
 
   //extractor
   static extract(String html) {
     List<FileInfo> links = [];
     String descriptionString = "";
     String thumbnailUrl = "";
-    String accountTagString = "";
+    String accountName = "";
     int jsonHeaderIndex = 0;
     int jsonFooterIndex = 0;
     bool json = false;
-    var detailsJsonDict = {};
     var valuesJsonDict = {};
-    int jsonIndex = 0;
     int linkStartIndex, linkEndIndex;
 
     for (int i = 0; i < html.length; i++) {
       //json LINKS
       if (!json) {
-        if (jsonHeader[0][jsonHeaderIndex] == html[i]) {
+        if (jsonHeader[jsonHeaderIndex] == html[i])
           jsonHeaderIndex++;
-          jsonIndex = 0;
-        } else if (jsonHeader[1][jsonHeaderIndex] == html[i]) {
+        else if (jsonHeader[jsonHeaderIndex] == html[i])
           jsonHeaderIndex++;
-          jsonIndex = 1;
-        } else
+        else
           jsonHeaderIndex = 0;
 
-        if (jsonHeader[jsonIndex].length == jsonHeaderIndex) {
+        if (jsonHeader.length == jsonHeaderIndex) {
           json = true;
           linkStartIndex = i + 1;
         }
       } else {
-        if (jsonFooter[jsonIndex][jsonFooterIndex] == html[i])
+        if (jsonFooter[jsonFooterIndex] == html[i])
           jsonFooterIndex++;
         else
           jsonFooterIndex = 0;
 
-        if (jsonFooterIndex == jsonFooter[jsonIndex].length) {
-          linkEndIndex = i - jsonFooter[jsonIndex].length + 1;
-          if (jsonIndex == 0)
-            detailsJsonDict =
-                jsonDecode(html.substring(linkStartIndex, linkEndIndex));
-          else
-            valuesJsonDict =
-                jsonDecode(html.substring(linkStartIndex, linkEndIndex));
+        if (jsonFooterIndex == jsonFooter.length) {
+          linkEndIndex = i - jsonFooter.length + 1;
+          valuesJsonDict =
+              jsonDecode(html.substring(linkStartIndex, linkEndIndex));
           json = false;
           jsonHeaderIndex = 0;
           jsonFooterIndex = 0;
@@ -61,33 +50,33 @@ class Extractor {
       }
     }
 
-    valuesJsonDict =
-        valuesJsonDict['entry_data']['PostPage'][0]['graphql']['shortcode_media'];
-    thumbnailUrl = valuesJsonDict['display_url'];
-    if(valuesJsonDict['is_video']) {
+    valuesJsonDict = valuesJsonDict['entry_data']['PostPage'][0]['graphql']
+        ['shortcode_media'];
+    thumbnailUrl = valuesJsonDict['display_resources'][0]['src'];
+    accountName = valuesJsonDict['owner']['username'];
+    if (valuesJsonDict['edge_media_to_caption']['edges'].length > 0)
+      descriptionString =
+          valuesJsonDict['edge_media_to_caption']['edges'][0]['node']['text'];
+
+    if (valuesJsonDict['is_video'])
       links.add(FileInfo(2, valuesJsonDict['video_url']));
-    }
+    else if (valuesJsonDict['edge_sidecar_to_children'] == null)
+      links.add(FileInfo(1, valuesJsonDict['display_url']));
     else {
       var valuesArray = valuesJsonDict['edge_sidecar_to_children']['edges'];
       for (int i = 0; i < valuesArray.length; i++) {
-        if (valuesArray[i]['node']['is_video']) {
+        if (valuesArray[i]['node']['is_video'])
           links.add(FileInfo(2, valuesArray[i]['node']['video_url']));
-        } else {
+        else
           links.add(FileInfo(1, valuesArray[i]['node']['display_url']));
-        }
       }
     }
 
-    if (detailsJsonDict['caption'] != null)
-      descriptionString = detailsJsonDict['caption'];
-    accountTagString = detailsJsonDict['author']['alternateName'];
-
-    var returnValue = {
+    return {
       "links": links,
       "description": descriptionString,
       "thumbnail_url": thumbnailUrl,
-      "account_tag": accountTagString,
+      "account_tag": accountName,
     };
-    return returnValue;
   }
 }
