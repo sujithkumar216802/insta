ebafb218f478f0d11750671b4e582ccd3c75081b
sujithkumar216802 <sujithkumar216802@gmail.com>
2022-03-18 01:53:23 +0530

    minor change to url check

--- lib/utils/url_checker.dart
+++ lib/utils/url_checker.dart
@@ -2,11 +2,14 @@ import '../enums/status_enum.dart';
 
 urlChecker(Uri uri) {
   String url;
-  if (uri.pathSegments.contains('stories') ||
-      uri.pathSegments.contains('s') ||
-      uri.pathSegments.contains('p') ||
-      uri.pathSegments.contains('tv') ||
-      uri.pathSegments.contains('reel')) {
+  if (uri.host == "www.instagram.com" &&
+      (uri.pathSegments.contains('stories') ||
+          uri.pathSegments.contains('s') ||
+          uri.pathSegments.contains('p') ||
+          uri.pathSegments.contains('tv') ||
+          uri.pathSegments.contains('reel')) &&
+      uri.pathSegments.length > 1 &&
+      uri.pathSegments[1].length > 0) {
     url = 'https://www.instagram.com' + uri.path;
     if (uri.queryParameters.containsKey('story_media_id') &&
         uri.pathSegments.contains('s'))
